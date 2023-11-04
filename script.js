fdc7278aae5939cbaec5dbcd73d7763cd57cde92
Darknhyt <40185738+Darknhyt@users.noreply.github.com>
2023-11-03 21:51:27 -0400

    New functions

--- script.js
+++ script.js
@@ -1,47 +1,175 @@
 const num = document.getElementById("keys");
 const disp = document.getElementById("screen");
-const buttons = "()+789-456*123/0.";
+const prev = document.getElementById("prev");
+const oper = document.getElementById("oper");
+
+let operation = false;
+const calc = {
+    val1: "",
+    val2: 0,
+    operator: "",
+    last: 0,
+    error: "No values",
+}
 
 init();
 function init(){
-    for (const w of buttons) {
-        addButton(w);
-    }
-    let e = document.createElement("button");
-    e.textContent = "=";
-    e.addEventListener("click",()=>{
-        getResult();
-    })
-    num.appendChild(e);
-}
-
-addEvents()
-function addButton(w){
-    let n = document.createElement("button");
-    n.textContent = w;
-    n.addEventListener("click",()=>{
-        disp.value += w;
-    })
-    num.appendChild(n);
+    display();
+    addEvents();
 }
 
 function addEvents(){
     num.children[0].addEventListener("click",()=>{
-        disp.value = "";
+        calc.val1 = calc.val2;
+        calc.val2 = calc.last;
+        calc.last = calc.val1;
+        if (calc.operator =="="){
+            calc.operator = "⇅";
+        }
+        display();
     })
     num.children[1].addEventListener("click",()=>{
+        if (!disp.value.startsWith("-")){
+            if (!disp.value.startsWith("0.")){
+                disp.value = disp.value.replace(/^0+/, "");
+            }
+            disp.value = "-" + disp.value;
+        } else {
+            disp.value = disp.value.slice(1);
+        }
+    })
+    num.children[2].addEventListener("click",()=>{
+        reset();
+        display();
+    })
+    num.children[3].addEventListener("click",()=>{
         disp.value = disp.value.slice(0,-1);
+    })
+    for (const w of num.getElementsByClassName("operator")) {
+        w.addEventListener("click", ()=>{
+         let number = disp.value;
+         if (operation == false){
+             calc.val1 = number;
+             calc.last = number;
+             calc.error = false;
+             operation = true;
+         }else{
+         calc.val1 = operate();
+         }
+         calc.operator = w.textContent;
+         calc.val2 = 0;
+         display();
+        })
+     }
+     disp.addEventListener("input",()=>{    
+         if (!disp.value.startsWith("0.")){
+             disp.value = disp.value.replace(/^0+/, "");
+         }
+     })
+     for (const n of num.getElementsByClassName("num")){
+         n.addEventListener("click",()=>{
+             if (!disp.value.startsWith("0.")){
+                 disp.value = disp.value.replace(/^0+/, "");
+             }
+             disp.value += n.textContent;
+             calc.val2 = disp.value;
+             calc.error = false;
+         })
+     }
+     document.getElementById("result").addEventListener("click", ()=>{        
+         if(calc.operator != "=" && getResult()){ 
+             calc.operator = "=";
+             operation = false;
+             display();
+         }  
+     })
+    document.getElementById("point").addEventListener("click", ()=>{
+        if(!disp.value.includes(".")){ 
+            disp.value = disp.value + ".";
+        }
     })
 }
 
 function getResult(){
     try {
-        let res = eval(disp.value); //Temporal code
-        disp.value = res;
-        disp.style.borderColor = "#FFF";
-        setTimeout(()=> disp.style.borderColor = "#0FF", 200);
+        let r = operate();
+        if(!calc.error){
+            calc.val1 = calc.val1 + calc.operator + calc.val2;
+            calc.last = calc.val2;
+            calc.val2 = r;
+            disp.style.borderColor = "#FFF";
+            setTimeout(()=> disp.style.borderColor = "#0FF", 200);
+            return true;
+        }else{
+            calError(calc.error);
+            return false
+        }
     } catch (error) { 
-        disp.style.borderColor = "#b44";
-        setTimeout(()=> disp.style.borderColor = "#0FF", 400);
+        console.log(error);
+        calError("ERROR");
     } 
+}
+
+function operate(){
+    let result = 0;
+    switch(calc.operator){
+        case "+": result = calSum(); break;
+        case "-": result = calRest(); break;
+        case "x": result = calProd(); break;
+        case "÷": result = calDiv(); break;
+        case "=": calc.error = "Invalid Operation"; break;
+        default : calError();
+    }
+    return result;
+}
+
+function calSum(){
+    return Number.parseFloat(calc.val1) + Number.parseFloat(calc.val2);
+}
+
+function calRest(){
+    return calc.val1 - calc.val2;
+}
+
+function calProd(){
+    return calc.val1 * calc.val2;
+}
+
+function calDiv(){
+    if (calc.val2 != 0){
+        return calc.val1 / calc.val2;
+    }else{
+        calc.error = "Divisor ≠ 0";
+        return calc.val1;
+    }
+}
+
+function calError(msj = "ERROR"){
+    oper.textContent = "!";
+    prev.value = msj;
+    oper.style.color = "#E00"
+    prev.style.color = "#E00";
+    disp.style.borderColor = "#E44";
+    setTimeout(()=> {
+        disp.style.borderColor = "#0FF";
+        oper.style.color = "#0FF"
+        prev.style.color = "#CCC";
+        display();
+    }, 2000);
+}
+
+function display() {
+    oper.textContent = calc.operator;
+    prev.value = calc.val1;
+    disp.value = calc.val2;
+}
+
+function reset(){
+    calc.val1 = "";
+    calc.val2 = 0;
+    calc.operator = "";
+    point = false;
+    last = 0;
+    error = false;
+    display();
 }
